1467d2d948c7dc771b1482f1b01487cf3daaac29
slim <slimabida21@gmail.com>
2025-09-09 11:48:25 +0200

    First commit initialisation project

---
+++ harmony-app/src/App.jsx
@@ -0,0 +1,29 @@
+"use client"
+import React from "react";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport
+} from "./components/ui/navigation-menu";
+
+export default function App() {
+  return (
+    <div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
