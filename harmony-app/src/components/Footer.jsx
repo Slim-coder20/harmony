6e67c823692a333e9c2ee7f9e360e638cc9c6c6e
slim <slimabida21@gmail.com>
2025-09-20 18:14:23 +0200

    finalisation de la section 2 du footer avec l'ajout Informations  Horrraires d'ouverture service client espace pro reseaux sociaux newsletter

--- harmony-app/src/components/Footer.jsx
+++ harmony-app/src/components/Footer.jsx
@@ -92,8 +92,112 @@ export default function Footer() {
         </button>
       </div>
       {/* Section informations - 2 colonnes  */}
-      <div className=" grid grid-cols-2"></div>
 
+      <div className=" grid grid-cols-2 md:grid-cols-1 lg:grid-cols-4 gap-8 mt-12">
+        {/* Informations et conditions générales */}
+        <div className="mt-10">
+          <h4 className="font-bold mb-2">INFORMATIONS</h4>
+          <ul className="text-sm text-gray-300 space-y-1">
+            <li>
+              <a href="/qui-sommes-nous">Qui sommes-nous ?</a>
+            </li>
+            <li>
+              <a href="/nos-magasins">Nos magasins</a>
+            </li>
+
+            <li>
+              <a href="/conditions-generales">Conditions générales de vente</a>
+            </li>
+            <li>
+              <a href="/mentions-legales">Mentions légales</a>
+            </li>
+            <li>
+              <a href="/politique-confidentialite">
+                Politique de confidentialité
+              </a>
+            </li>
+          </ul>
+        </div>
+        {/* Horraires d'ouverture */}
+        <div className="mt-10">
+          <h4 className="font-bold mb-2">HORAIRES D'OUVERTURE</h4>
+          <ul className="text-sm text-gray-300 space-y-1">
+            <li>Lundi - Vendredi: 10h - 19h</li>
+            <li>Samedi: 10h - 18h</li>
+            <li>Dimanche: Fermé</li>
+          </ul>
+        </div>
+        {/* Service client */}
+        <div className="mt-10">
+          <h4 className="font-bold mb-2">SERVICE CLIENT</h4>
+          <ul className="text-sm text-gray-300 space-y-1">
+            <li>
+              <a href="/livraison">Livraison</a>
+            </li>
+            <li>
+              <a href="/retours">Retours</a>
+            </li>
+            <li>
+              <a href="/contact">Contactez-nous</a>
+            </li>
+          </ul>
+        </div>
+        {/* Informations */}
+        <div className="mt-10">
+          <h4 className="font-bold mb-2">ESPACE PRO</h4>
+          <ul className="text-sm text-gray-300 space-y-1">
+            <li>Devenir partenaire</li>
+            <li>Demande de devis</li>
+          </ul>
+        </div>
+        {/* Reseaux sociaux */}
+        <div className="mt-10">
+          <h4 className="font-bold mb-2">SUIVEZ-NOUS</h4>
+          <ul className="text-sm text-gray-300 space-y-1">
+            <li>
+              <a href="https://www.facebook.com">Facebook</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com">Instagram</a>
+            </li>
+            <li>
+              <a href="https://www.twitter.com">Twitter</a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com">YouTube</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com">LinkedIn</a>
+            </li>
+          </ul>
+        </div>
+        {/* newsletter */}
+        <div className="mt-10">
+          <h4 className="font-bold mb-2">NEWSLETTER</h4>
+          <p className="text-sm text-gray-300 mb-2">
+            Inscrivez-vous à notre newsletter pour recevoir les dernières
+            actualités et offres.
+          </p>
+          <form className="flex">
+            <input
+              type="email"
+              placeholder="Votre email"
+              className="flex-1 p-2 border border-gray-300 rounded-l"
+            />
+            <button className="bg-blue-600 hover:bg-blue-700 text-white p-2 rounded-r cursor-pointer">
+              S'inscrire
+            </button>
+          </form>
+        </div>
+        {/* Logo */}
+        <div className="mt-10">
+          <img
+            src="/Logo.png"
+            alt="Logo"
+            className="mx-auto w-32 flex items-center"
+          />
+        </div>
+      </div>
     </footer>
   );
 }
