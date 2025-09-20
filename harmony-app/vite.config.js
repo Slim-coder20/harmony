603c154acd41d972359966595a8c871843e8da64
slim <slimabida21@gmail.com>
2025-09-20 22:07:33 +0200

    dockerisation du projet avec création de l'image harmony-app et mise en place du docker-compose.yml test OK

--- harmony-app/vite.config.js
+++ harmony-app/vite.config.js
@@ -7,7 +7,14 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(new URL('.', import.meta.url).pathname, "src"),
+      "@": path.resolve(new URL(".", import.meta.url).pathname, "src"),
+    },
+  },
+  server: {
+    host: "0.0.0.0", // Important pour Docker
+    port: 5173,
+    watch: {
+      usePolling: true, // Pour le hot reload dans Docker
     },
   },
 });
