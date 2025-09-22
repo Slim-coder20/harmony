4b962e7db8f34b69e767cdce09d3c6472e916516
slim <slimabida21@gmail.com>
2025-09-22 18:15:14 +0200

    Ajout d'un lien pour retour vers le login user

--- harmony-app/src/pages/connexion/ForgotPassword.jsx
+++ harmony-app/src/pages/connexion/ForgotPassword.jsx
@@ -52,11 +52,13 @@ export default function ForgotPassword() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
-          Réinitialiser votre mot de passe
+        <h2 className="mt-6  text-5xl font-bold text-gray-900">
+          Mot de passe oublié
         </h2>
-        <p className="mt-2 text-center text-sm text-gray-600">
-          Entrez votre email pour recevoir un lien de réinitialisation
+        <p className="mt-2 text-center text-2xl text-gray-600">
+          Si vous avez oublié votre mot de passe, entrez votre adresse
+          électronique ci-dessous et nous vous enverrons un courrier
+          électronique vous permettant de créer un nouveau mot de passe.
         </p>
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
@@ -90,7 +92,7 @@ export default function ForgotPassword() {
                 type="submit"
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-200 cursor-pointer"
               >
-                Réinitialiser mot de passe
+                Nouveau mot de passe
               </button>
             </div>
           </form>
