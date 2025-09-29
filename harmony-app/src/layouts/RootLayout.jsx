8f93e92ac71cdfeda544864054b55db59ba063c4
slim <slimabida21@gmail.com>
2025-09-29 13:25:05 +0200

    ajout du ToastProvider dans le RootLayout

--- harmony-app/src/layouts/RootLayout.jsx
+++ harmony-app/src/layouts/RootLayout.jsx
@@ -1,15 +1,21 @@
 import Header from "@/components/Header";
 import FooterFixed from "@/components/FooterFixed";
 import Nav from "@/components/Nav";
+import CartProvider from "@/context/CartContext";
 import { Outlet } from "react-router-dom";
+import { ToastProvider, Toasts } from "@/context/ToastContext";
 
 export default function RootLayout() {
   return (
-    <>
-      <Header />
-      <Nav />
-      <Outlet />
-      <FooterFixed />
-    </>
+      <ToastProvider>
+        <CartProvider>
+          <Header />
+          <Nav />
+          <Outlet/>
+          <FooterFixed />
+        </CartProvider>
+        <Toasts />
+      </ToastProvider>
+      
   );
 }
