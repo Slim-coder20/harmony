dfdad229f5ed53b2c4934acfb20c9885b82a1b1b
slim <slimabida21@gmail.com>
2025-09-30 00:34:15 +0200

    englober App.jsx par le ToastProvider et le wishlistProvider

--- harmony-app/src/main.jsx
+++ harmony-app/src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import { ToastProvider } from "./context/ToastContext.jsx";
+import WishlistProvider from "./context/WishlistContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <ToastProvider> 
+      <WishlistProvider>
       <App />
-    </BrowserRouter>
+      </WishlistProvider>
+    </ToastProvider>
+      </BrowserRouter>
   </StrictMode>
 );
